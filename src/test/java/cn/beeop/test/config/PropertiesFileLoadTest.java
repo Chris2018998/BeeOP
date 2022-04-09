60b516b99a3c4b3de7582f9f677269577bb4a9b7
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2022-04-09 14:25:29 +0800

    Add files via upload

--- src/test/java/cn/beeop/test/config/PropertiesFileLoadTest.java
+++ src/test/java/cn/beeop/test/config/PropertiesFileLoadTest.java
@@ -12,44 +12,44 @@ import cn.beeop.test.TestCase;
 
 import java.net.URL;
 
+import static cn.beeop.pool.PoolStaticCenter.isBlank;
+
 /**
  * @author Chris.Liao
  * @version 1.0
  */
 public class PropertiesFileLoadTest extends TestCase {
 
-    private static void checkBase(BeeObjectSourceConfig config) {
-        if (!"Pool1".equals(config.getPoolName())) throw new BeeObjectSourceConfigException("poolName error");
-        if (!config.isFairMode()) throw new BeeObjectSourceConfigException("fairMode error");
-        if (config.getInitialSize() != 1) throw new BeeObjectSourceConfigException("initialSize error");
-        if (config.getMaxActive() != 10) throw new BeeObjectSourceConfigException("maxActive error");
-        if (config.getBorrowSemaphoreSize() != 4) throw new BeeObjectSourceConfigException("borrowSemaphoreSize error");
+    private static String checkBase(BeeObjectSourceConfig config) {
+        if (!"Pool1".equals(config.getPoolName())) return "poolName error";
+        if (!config.isFairMode()) return "fairMode error";
+        if (config.getInitialSize() != 1) return "initialSize error";
+        if (config.getMaxActive() != 10) return "maxActive error";
+        if (config.getBorrowSemaphoreSize() != 4) return "borrowSemaphoreSize error";
+        return null;
     }
 
-    private static void checkTimeValue(BeeObjectSourceConfig config) {
-        if (config.getMaxWait() != 8000) throw new BeeObjectSourceConfigException("maxWait error");
-        if (config.getIdleTimeout() != 18000) throw new BeeObjectSourceConfigException("idleTimeout error");
-        if (config.getHoldTimeout() != 30000) throw new BeeObjectSourceConfigException("holdTimeout error");
-        if (config.getValidTestTimeout() != 3) throw new BeeObjectSourceConfigException("objectTestTimeout error");
-        if (config.getValidAssumeTime() != 500)
-            throw new BeeObjectSourceConfigException("objectTestInterval error");
-        if (config.getTimerCheckInterval() != 30000)
-            throw new BeeObjectSourceConfigException("idleCheckTimeInterval error");
-        if (!config.isForceCloseUsingOnClear())
-            throw new BeeObjectSourceConfigException("forceCloseUsingOnClear error");
-        if (config.getDelayTimeForNextClear() != 3000)
-            throw new BeeObjectSourceConfigException("delayTimeForNextClear error");
+    private static String checkTimeValue(BeeObjectSourceConfig config) {
+        if (config.getMaxWait() != 8000) return "maxWait error";
+        if (config.getIdleTimeout() != 18000) return "idleTimeout error";
+        if (config.getHoldTimeout() != 30000) return "holdTimeout error";
+        if (config.getValidTestTimeout() != 3) return "objectTestTimeout error";
+        if (config.getValidAssumeTime() != 500) return "objectTestInterval error";
+        if (config.getTimerCheckInterval() != 30000) return "idleCheckTimeInterval error";
+        if (!config.isForceCloseUsingOnClear()) return "forceCloseUsingOnClear error";
+        if (config.getDelayTimeForNextClear() != 3000) return "delayTimeForNextClear error";
+        return null;
     }
 
-    private static void checkInterface(BeeObjectSourceConfig config) {
-        if (!"cn.beeop.test.object.JavaBook".equals(config.getObjectClassName()))
-            throw new BeeObjectSourceConfigException("objectClassName error");
-        //if(!"cn.beeop.test.object.JavaBook".equalsString(config.getObjectClass().getClass().getName()))throw new BeeObjectSourceConfigException("objectClass error");
+    private static String checkInterface(BeeObjectSourceConfig config) {
+        if (!"cn.beeop.test.object.JavaBook".equals(config.getObjectClassName())) return "objectClassName error";
+        //if(!"cn.beeop.test.object.JavaBook".equalsString(config.getObjectClass().getClass().getName()))return"objectClass error";
         if (!"cn.beeop.test.object.JavaBookFactory".equals(config.getObjectFactoryClassName()))
-            throw new BeeObjectSourceConfigException("objectFactoryClassName error");
+            return "objectFactoryClassName error";
         if (!"cn.beeop.pool.FastObjectPool".equals(config.getPoolImplementClassName()))
-            throw new BeeObjectSourceConfigException("objectFactoryClassName error");
-        if (!config.isEnableJmx()) throw new BeeObjectSourceConfigException("enableJmx error");
+            return "objectFactoryClassName error";
+        if (!config.isEnableJmx()) return "enableJmx error";
+        return null;
     }
 
     public void testConfig() {
@@ -59,8 +59,11 @@ public class PropertiesFileLoadTest extends TestCase {
         BeeObjectSourceConfig config = new BeeObjectSourceConfig();
         config.loadFromPropertiesFile(url.getFile());
 
-        checkBase(config);
-        checkTimeValue(config);
-        checkInterface(config);
+        String error = checkBase(config);
+        if (!isBlank(error)) throw new BeeObjectSourceConfigException(error);
+        error = checkTimeValue(config);
+        if (!isBlank(error)) throw new BeeObjectSourceConfigException(error);
+        error = checkInterface(config);
+        if (!isBlank(error)) throw new BeeObjectSourceConfigException(error);
     }
 }
